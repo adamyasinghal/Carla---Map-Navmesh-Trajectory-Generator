2a0c753fa4d6374035644b4f954a4b37b03a888a
adamyasinghal <adamyasinghal2006@gmail.com>
2026-06-25 21:28:30 +0530

    Completed genereate_navmesh.py

--- generate_navmesh.py
+++ generate_navmesh.py
@@ -1,14 +1,13 @@
 import carla
-import random
 import numpy as np
 
 resolution = 0.5 #in meters, can tweaked for a tighter grid but will be more computationally expensive
 
 #grid size, can be tweaked
-xmin = -300 
-xmax = 300
-ymin = -300
-ymax = 300
+xmin = -200 
+xmax = 200
+ymin = -200
+ymax = 200
 
 #ray parameters
 ray_start_height = 100.0
@@ -22,13 +21,14 @@ ALLOWED_LABELS = {
     carla.CityObjectLabel.Shoulder,
 }
 
-carla = carla.Client('localhost', 2000)
+client = carla.Client('localhost', 2000)
 client.set_timeout(20.0)
 world = client.get_world()
 
-print("Connected to : ", world.get_map.name)
+print("Connected to : ", world.get_map().name)
 
-valid_hits = []
+#valid_hits = [] #commented out for now
+grid = {}
 
 num_x = len(np.arange(xmin, xmax, resolution))
 num_y = len(np.arange(ymin, ymax, resolution))
@@ -58,13 +58,78 @@ for x in np.arange(xmin, xmax, resolution):
         hit = hits[0]
         
         label = hit.label
-        if label not in ALLOWED_LABELS:
-            continue
 
-        valid_hits.append([
+        ix = int(round((x - xmin) / resolution))
+        iy = int(round((y - ymin) / resolution))
+
+        #if label not in ALLOWED_LABELS:
+        #    continue
+        #uncomment later
+
+        grid[(ix, iy)] = {
+        "location": np.array([hit.location.x, hit.location.y, hit.location.z], dtype=np.float32),
+        "label": label,
+        "normal": np.array([hit.normal.x, hit.normal.y, hit.normal.z], dtype=np.float32),
+}
+
+        #commented out for now
+        '''
+            valid_hits.append([
             hit.location.x,
             hit.location.y,
             hit.location.z
         ])
+        '''
 
-print(f"Total ray hists : {len(valid_hits)}")
+print(f"Total ray hits : {len(grid)}")
+
+clearance = 1.5 #in meters, can be tweaked depending on size of ego vehicle
+radius = int(np.ceil(clearance / resolution))
+
+safe_points = []
+
+for (ix, iy), cell in grid.items():
+
+    if cell["label"] not in ALLOWED_LABELS:
+        continue
+
+    keep = True
+
+    for dx in range(-radius, radius + 1):
+        for dy in range(-radius, radius + 1):
+
+            if dx*dx + dy*dy > radius*radius:
+                continue
+
+            neighbor = grid.get((ix + dx, iy + dy))
+
+            if neighbor is None:
+                keep = False
+                break
+
+            if neighbor["label"] not in ALLOWED_LABELS:
+                keep = False
+                break
+
+        if not keep:
+            break
+
+    if keep:
+        normal = cell["normal"]
+        loc = cell["location"]
+
+        safe_points.append(np.concatenate((loc, normal)))
+
+print(f"Safe points: {len(safe_points)}")
+
+safe_points = np.array(safe_points, dtype=np.float32)
+
+np.save("navmesh.npy", safe_points)
+
+for p in safe_points:
+    world.debug.draw_point(
+        carla.Location(x=p[0], y=p[1], z=p[2]),
+        size=0.05,
+        color=carla.Color(0, 255, 0),
+        life_time=60.0
+    )
